9189403708b12f4f26e3188344f71b426ababe54
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-07-14 03:40:20 +0530

    api-gateway added for routing purpuse and added configurations

---
+++ LinkedIn-Project/APIGateway/src/test/java/com/soumyajit/LinkedInMicroservice/APIGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soumyajit.LinkedInMicroservice.APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
