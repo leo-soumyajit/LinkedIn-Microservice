64c1347cb309b0304e3bd20bfedbe8247d955f96
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-07-30 01:29:22 +0530

    Controller code parameter change : from @RequestParam -> @RequestPart

--- LinkedIn-Project/uploader-service/src/main/java/com/soumyajit/LinkedInMicroservice/uploader_service/Controller/Controller.java
+++ LinkedIn-Project/uploader-service/src/main/java/com/soumyajit/LinkedInMicroservice/uploader_service/Controller/Controller.java
@@ -1,13 +1,10 @@
 package com.soumyajit.LinkedInMicroservice.uploader_service.Controller;
 
-import com.soumyajit.LinkedInMicroservice.uploader_service.Advices.ApiResponse;
 import com.soumyajit.LinkedInMicroservice.uploader_service.Service.CloudeneryUploaderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -17,11 +14,12 @@ public class Controller {
 
     private final CloudeneryUploaderService cloudeneryUploaderService;
 
-    @PostMapping
-    public ResponseEntity<ApiResponse<String>> upload(@RequestParam
+    @PostMapping(
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    public ResponseEntity<String> upload(@RequestPart("multipartFile")
                                              MultipartFile multipartFile){
         String url = cloudeneryUploaderService.uploader(multipartFile);
-        ApiResponse apiResponse = new ApiResponse(url);
-        return ResponseEntity.ok(apiResponse);
+        return ResponseEntity.ok(url);
     }
 }
